05db111efff8a6be6ff24be6f2178955f34c6b75
Sami Karkinen <sami.karkinen@tuni.fi>
2020-01-26 13:01:28 +0200

    Made analysisParse into func and made a more complete test of how it works

--- Analysis_parser.py
+++ Analysis_parser.py
@@ -1,4 +1,18 @@
 from pandas import read_json, DataFrame
 
-df = read_json('analysis.json')
-df.to_csv('analysis.csv')
+## analysisParse
+#  @brief Makes a CSV file from the JSON
+#  @param json The json as string
+def analysisParse(json):
+    df = read_json(json, orient='index')
+    df.to_csv('analysis.csv')
+
+if __name__ == "__main__":
+
+    # Making a test json and reading it
+    json = '{"row 1":{"col 1":"a","col 2":"b"},"row 2":{"col 1":"c","col 2":"d"}}'
+    df = read_json(json,orient='index')
+
+    # Print the dataframe and make it into CSV
+    print(df)
+    df.to_csv('test.csv')
