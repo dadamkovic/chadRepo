6ea9262ff036499d8ec318b88e3cf2b3c09c8804
Vesa Koskela <vesa.koskela@tuni.fi>
2020-02-06 14:24:10 +0200

    Implemented methods for checking sonarqube status and for stopping it.

--- sonarStuff.py
+++ sonarStuff.py
@@ -1,36 +1,50 @@
 import docker
 import requests
+import time
 
-
+##  Sonar
+#   @brief class for handling SonarQube and SonarScanner for Maven
 class Sonar:
+
+    ##  The Constructor
     def __init__(self):
         self.client = docker.from_env()
-        self.sonarQubeContainer = "Null"
+        self.sonarQubeContainer = None
         self.sonarQubeRunning = False
 
+    ##  startSonarQube
+    #   @brief Method for starting sonarQube docker container
+    #   SonarQube server is not up yet after calling this method.
+    #   Use isSonarQubeRunning to check if it's up.
     def startSonarQube(self):
-        print("starting sonarqube")
+        print("SonarQube is starting..")
         if self.isSonarQubeRunning() is False:
-            print("sonar")
             self.sonarQubeContainer = self.client.containers.run(
-                "sonarqube", detach=True,
+                "sonarqube",
+                detach=True,
                 name="sonarqube",
                 ports={'9000/tcp': 9000}
             )
-            """
-            for line in self.sonarQubeContainer.logs(stream=True):
-                print(line.strip())
-                """
 
+    ##  stopSonarQube
+    #   @brief Method for stopping SonarQube
+    #   Stops SonarQube docker container and removes it
     def stopSonarQube(self):
-        self.sonarQubeContainer.stop()
+        self.sonarQubeContainer.stop(timeout=3600)
 
+        while self.sonarQubeContainer.status != "exited":
+            self.sonarQubeContainer.reload()
+            time.sleep(1)
+        self.sonarQubeContainer.remove()
+        self.sonarQubeRunning = False
+
+    ##  isSonarQubeRunning
+    #   @brief Method for checking if SonarQube server is up
+    #
+    #   @returns True only when SonarQube server HTTP response is 200
     def isSonarQubeRunning(self):
-        print("is sonar running")
-
         try:
-            r = requests.head("http://127.0.0.1")
-            print(r.status_code)
+            r = requests.head("http://127.0.0.1:9000")
             if r.status_code == 200:
                 self.sonarQubeRunning = True
                 return True
@@ -38,9 +52,15 @@ class Sonar:
                 self.sonarQubeRunning = False
                 return False
         except requests.ConnectionError:
-            print("failed to connect")
-        return False
+            return False
 
 
+"""
 sonar = Sonar()
 sonar.startSonarQube()
+while sonar.isSonarQubeRunning() is False:
+    print(".", sep="", end="", flush=True)
+    time.sleep(2)
+print("SonarQube is Up!")
+sonar.stopSonarQube()
+"""
